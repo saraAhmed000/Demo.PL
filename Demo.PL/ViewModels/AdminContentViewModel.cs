676ef149c428b7c6bfab1d6268df5a90e52ff197
sarahahmed21 <96779524+sarahahmed21@users.noreply.github.com>
2024-05-03 21:13:03 +0300

    Routing with views

---
+++ Demo.PL/ViewModels/AdminContentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo.PL.ViewModels
+{
+    public class AdminContentViewModel
+    {
+
+        
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+        
+
+    }
+}
